c6d0f7ae59fd7942faa38f88facc09dde902fa77
Anton Belousov <antonbelousovwork@gmail.com>
2023-10-28 01:03:31 +0400

    добавил переходы,  кнопки выбора уровня недоделал

---
+++ js/action_button.js
@@ -0,0 +1,36 @@
+"use strict"; //сертификат для проверки ошибок
+const butRating = document.querySelector("#rating");
+const butSettings = document.querySelector("#settings");
+const butAbout = document.querySelector("#about");
+const butFaq = document.querySelector("#faq");
+const butLvls = [
+  document.getElementById("#lvl1"),
+  document.getElementById("#lvl2"),
+  document.getElementById("#lvl3"),
+];
+
+butRating.addEventListener("click", () => {
+  window.location.href = "player_rating.html";
+});
+
+butSettings.addEventListener("click", () => {
+  window.location.href = "player_settings.html";
+});
+
+butAbout.addEventListener("click", () => {
+  alert(
+    "Веб-приложение игра 'Тетрис' с функциями администратора\nО разработчиках.\nВыполнили: 6402 Белоусов Христич\nКурс: \"Технологии программирования\"\nПреподватель: Зеленко Лариса Сергеевна"
+  );
+});
+/*
+butLvls[0].addEventListener("click", () => {
+  butLvls[0].style.color = "white";
+});
+
+const butLvls1 = document.getElementById("#lvl1");
+console.log(butLvls1);
+
+butLvls1.addEventListener("click", () => {
+  butLvls1.style.backgroundColor = "white";
+});
+*/
