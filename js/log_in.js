09ab19e59d2d85d3fff3b14652b866d00742b860
Anton Belousov <antonbelousovwork@gmail.com>
2023-10-27 12:09:13 +0400

    Неправильный файл log_in_2

--- js/log_in.js
+++ js/log_in.js
@@ -1,24 +1,22 @@
-//console.log("Я программист");
+//кнопка войти
+const but_logIn = document.querySelector("#log_in_but");
+//поле логина
+const log_inp = document.querySelector("#log_inp");
+//поле пароля
+const pass_inp = document.querySelector("#pass_inp");
 
-class user {
-  constructor(login, password) {
-    this.login = login;
-    this.password = password;
+function checkData() {
+  let userLogin = log_inp.value;
+  let userPassword = pass_inp.value;
+  if (
+    userLogin <= 16 &&
+    userLogin >= 4 &&
+    userPassword <= 16 &&
+    userPassword >= 4
+  ) {
+    return true;
   }
+  return false;
 }
-let user = {
-  login: "",
-  password: "",
-  validateDataOnLen: function () {
-    if (
-      this.login.length >= 4 &&
-      this.login.length <= 12 &&
-      this.password.length >= 4 &&
-      this.password.length <= 12
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  },
-};
+
+but_logIn.addEventListener("click", () => checkData());
