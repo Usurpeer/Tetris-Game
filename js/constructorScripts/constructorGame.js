9d38ef73db77522752da3a9e77f897347b33b528
Anton Belousov <antonbelousovwork@gmail.com>
2023-11-19 20:21:58 +0400

    Все проверки для конструктора.

--- js/constructorScripts/constructorGame.js
+++ js/constructorScripts/constructorGame.js
@@ -1,86 +1,92 @@
 export default class ConstructorGame {
-  constructor(allfiguresString, newFigure) {
-    this.allfiguresString = allfiguresString;
-    this.newFig = this.getArrayFigure(newFigure);
-    this.countFigures = allfiguresString.length;
-
-    //this.checkUnique();
+  constructor(allFiguresString, newFigure) {
+    this.allFiguresString = allFiguresString;
+    this.newFigure = this.getArrayFromString(newFigure);
+    this.countFigures = allFiguresString.length;
   }
 
-  newFig = [];
-  allfiguresString;
+  newFigure = [];
+  allFiguresString;
   countFigures = 0;
 
   // инициализация массива новой фигуры в
-  getArrayFigure(stringFig) {
+  getArrayFromString(stringFig) {
     // преобразование строки в двумерный массив 4*4
-    let arrayFig = [];
+    let arr = [];
     for (let i = 0; i < 4; i++) {
-      arrayFig[i] = [];
+      arr[i] = [];
       for (let j = 0; j < 4; j++) {
-        arrayFig[i][j] = stringFig[i * 3 + j + i];
+        arr[i][j] = stringFig[i * 3 + j + i];
       }
     }
-    return arrayFig;
+    return arr;
   }
 
   // итоговый метод, который вызывается и возвращает True/False - результат общей проверки
-  allChecks() {}
-  // проверка уникальности сдвиг и повороты и сравнение
-  checkUnique() {
-    let check = 0; // если будет единица, значит фигура не уникальна
+  isCorrect() {
+    if (this.isUnique() == 1 && this.isWholeness() == true) {
+      return true;
+    }
+    return false;
+  }
+
+  // проверка уникальности с массивом имеющихся фигур сдвиг, повороты и сравнение
+  isUnique() {
+    let uniq = 1; // если будет единица, значит фигура уникальна
 
     for (let i = 0; i < this.countFigures; i++) {
       // получили фигуру, её надо сравнить с newFig поворотами, сдвигами
-      let figure = this.getArrayFigure(this.allfiguresString[i] + "");
-      console.log(figure);
+      let arraySavedFigure = this.getArrayFromString(
+        this.allFiguresString[i] + ""
+      );
+
       // сдвинули фигуры в левых вернхий угол
-      figure = this.moveInEmpty(figure);
-      this.newFig = this.moveInEmpty(this.newFig);
+      arraySavedFigure = this.moveInEmpty(arraySavedFigure);
+      this.newFigure = this.moveInEmpty(this.newFigure);
 
       // надо сравнить в текущем положении
-      check = this.compareFig(figure, this.newFig);
-      if (check == 1) {
-        return 1;
+      uniq = this.compareArrays(arraySavedFigure, this.newFigure);
+      if (uniq == 0) {
+        return 0;
       }
 
-      // надо повернуть одну из фигур, например newFig и сравнить
-      this.newFig = this.rotateFig(this.newFig);
-      check = this.compareFig(figure, this.newFig);
-      if (check == 1) {
-        return 1;
+      this.newFigure = this.rotateFig(this.newFigure);
+      uniq = this.compareArrays(arraySavedFigure, this.newFigure);
+      if (uniq == 0) {
+        return 0;
       }
 
-      // надо повернуть одну из фигур, например newFig и сравнить
-      this.newFig = this.rotateFig(this.newFig);
-      check = this.compareFig(figure, this.newFig);
-      if (check == 1) {
-        return 1;
+      this.newFigure = this.rotateFig(this.newFigure);
+      uniq = this.compareArrays(arraySavedFigure, this.newFigure);
+      if (uniq == 0) {
+        return 0;
       }
 
-      // надо повернуть одну из фигур, например newFig и сравнить
-      this.newFig = this.rotateFig(this.newFig);
-      check = this.compareFig(figure, this.newFig);
-      if (check == 1) {
-        return 1;
+      this.newFigure = this.rotateFig(this.newFigure);
+      uniq = this.compareArrays(arraySavedFigure, this.newFigure);
+      if (uniq == 0) {
+        return 0;
       }
     }
-    return check;
+    if (uniq == 1) {
+      console.log("Фигура уникальна.");
+    } else {
+      console.log("Фигура не уникальная.");
+    }
+    return uniq;
   }
-
-  // сравнивает два массива, в случае совпадения возвращает 1
-  compareFig(fig1, fig2) {
-    let check = 1;
+  // сравнивает два массива, в случае совпадения возвращает 0
+  compareArrays(fig1, fig2) {
+    let check = 0;
     for (let i = 0; i < fig1.length; i++) {
       for (let j = 0; j < fig1[i].length; j++) {
         if (fig1[i][j] != fig2[i][j]) {
-          check = 0;
+          check = 1;
         }
       }
     }
     return check;
   }
-
   // метод, который сдвигает фигуру в левый верхний угол, если там свободно
   moveInEmpty(form) {
     let checkEmptyY = [0, 0, 0, 0]; // счетчик пустых клеток, индекс - строка
@@ -127,7 +133,6 @@ export default class ConstructorGame {
 
     return form;
   }
-
   // поворот фигуры со сдвигом
   rotateFig(fig) {
     let tempForm = [];
@@ -140,11 +145,137 @@ export default class ConstructorGame {
     return this.moveInEmpty(tempForm);
   }
 
-  // проверка целостности флаг что рядом есть вполтную часть фигуры
-  checkwholeness() {
-    let arrayPassedCells = []; // здесь будут сохраняться клетки, которые были пройдены
+  // проверка целостности
+  isWholeness() {
+    let checkedFigure = [];
+    let length = [1]; // массив, чтобы длина передавалась ссылкой, начинается с двух
+    this.arrayCopy(checkedFigure); // копирование массива, чтобы не изменять исходник.
+    for (let i = 0; i < 4; i++) {
+      // начало отсчета длины от первой единицы в верхей строке (необходимо, чтобы фигура была сдвинута вверх)
+      if (checkedFigure[0][i] == "1") {
+        this.getLengthFigure(0, i, checkedFigure, length);
+        break;
+      }
+    }
+
+    // если в получившемся массиве есть несвязные клетки, то есть их значение осталось 1, то минус
+    for (let i = 0; i < checkedFigure.length; i++) {
+      for (let j = 0; j < checkedFigure[i].length; j++) {
+        if (checkedFigure[i][j] == 1) {
+          return false;
+        }
+      }
+    }
+    console.log("Успешная проверка на целостность.");
+    return this.isNoEmpty(checkedFigure);
+  }
+  // составление длины рекурсивным методом
+  getLengthFigure(i, j, chekedFigure, currentLength) {
+    if (chekedFigure[i][j] != undefined && chekedFigure[i][j] == 1) {
+      currentLength[0]++;
+      chekedFigure[i][j] = currentLength[0];
+
+      // вправо
+      if (chekedFigure[i].length > j + 1 && chekedFigure[i][j + 1] == 1) {
+        this.getLengthFigure(i, j + 1, chekedFigure, currentLength);
+      }
+      // назад
+      if (chekedFigure[i].length - (j - 1) > 0 && chekedFigure[i][j - 1] == 1) {
+        this.getLengthFigure(i, j - 1, chekedFigure, currentLength);
+      }
+      // вниз, под текущем положением
+      if (chekedFigure.length > i + 1 && chekedFigure[i + 1][j] == 1) {
+        this.getLengthFigure(i + 1, j, chekedFigure, currentLength);
+      }
+      // вверх
+      if (i - 1 >= 0 && chekedFigure[i - 1][j] == 1) {
+        this.getLengthFigure(i - 1, j, chekedFigure, currentLength);
+      }
+      // первый столбец
+      /*if (chekedFigure.length > i + 1 && chekedFigure[i + 1][j] == 1) {
+        this.getLengthFigure(i + 1, 0, chekedFigure, currentLength);
+      }*/
+    }
+  }
+  arrayCopy(newArray) {
+    for (let i = 0; i < this.newFigure.length; i++) {
+      newArray[i] = [];
+      for (let j = 0; j < this.newFigure[i].length; j++) {
+        newArray[i][j] = this.newFigure[i][j];
+      }
+    }
   }
 
-  // проверка на пустоту внутри, герметичность ????
-  checkIntegrity() {}
+  // проверка на пустоту
+  isNoEmpty(checkedFigure) {
+    this.setBorders(checkedFigure);
+
+    this.getArrayEmptiness(1, 1, checkedFigure); // стартовый индекс сразу 1,1
+
+    // проверка, если осталось хоть 1 нулевое значение, значит оно закрыто от границ, значит false
+    for (let i = 0; i < checkedFigure.length; i++) {
+      for (let j = 0; j < checkedFigure[i].length; j++) {
+        if (checkedFigure[i][j] == 0) {
+          console.log("Фигура содержит пустоты.");
+          return false;
+        }
+      }
+    }
+    console.log("Фигура не содержит пустот.");
+    return true;
+  }
+  //на вход - границы и массив, надо проверить есть ли пососедству -1, если есть, то тоже -1. Тогда если в массиве не содержится нулей, то нет пустот
+  getArrayEmptiness(i, j, checkedFigure) {
+    if (checkedFigure[i][j] == 0) {
+      // проверить сверху
+      if (checkedFigure[i - 1][j] == -1) {
+        checkedFigure[i][j] = -1;
+      }
+      // проверить справа
+      else if (checkedFigure[i][j + 1] == -1) {
+        checkedFigure[i][j] = -1;
+      }
+      // проверить снизу
+      else if (checkedFigure[i + 1][j] == -1) {
+        checkedFigure[i][j] = -1;
+      }
+      // проверить слева
+      else if (checkedFigure[i][j - 1] == -1) {
+        checkedFigure[i][j] = -1;
+      }
+    }
+    // вправо
+    if (checkedFigure[i].length - 1 > j + 1 && checkedFigure[i][j + 1] == 0) {
+      this.getArrayEmptiness(i, j + 1, checkedFigure);
+    }
+    // назад
+    if (checkedFigure[i].length - j > 0 && checkedFigure[i][j - 1] == 0) {
+      this.getArrayEmptiness(i, j - 1, checkedFigure);
+    }
+    // вниз, под текущем положением
+    if (checkedFigure.length - 1 > i + 1 && checkedFigure[i + 1][j] == 0) {
+      this.getArrayEmptiness(i + 1, j, checkedFigure);
+    }
+    // вверх
+    if (i - 1 > 1 && checkedFigure[i - 1][j] == 0) {
+      this.getArrayEmptiness(i - 1, j, checkedFigure);
+    }
+  }
+  // задает -1 для значений на границах, для того, чтобы задать связь между внутренними
+  setBorders(checkedFigure) {
+    for (let i = 0; i < checkedFigure.length; i++) {
+      if (checkedFigure[i][0] == 0) {
+        checkedFigure[i][0] = -1;
+      }
+      if (checkedFigure[0][i] == 0) {
+        checkedFigure[0][i] = -1;
+      }
+      if (checkedFigure[checkedFigure.length - 1][i] == 0) {
+        checkedFigure[checkedFigure.length - 1][i] = -1;
+      }
+      if (checkedFigure[i][checkedFigure.length - 1] == 0) {
+        checkedFigure[i][checkedFigure.length - 1] = -1;
+      }
+    }
+  }
 }
--- js/constructorScripts/initConstructor.js
+++ js/constructorScripts/initConstructor.js
@@ -1,35 +1,38 @@
 import ConstructorGame from "./constructorGame.js";
 
 const allFigures = [
-    ["A000A000A000A000"], // линия
-    ["BB00BB0000000000"], // квадрат
-    ["0C00CCC000000000"], // T образная
-    ["0DD0DD0000000000"], // S
-    ["EE000EE000000000"], // Z
-    ["F000F000FF000000"], // L
-    ["0G000G00GG000000"], // Other L
+    ["1000100010001000"], // линия
+    ["1100110000000000"], // квадрат
+    ["0100111000000000"], // T образная
+    ["0110110000000000"], // S
+    ["1100011000000000"], // Z
+    ["1000100011000000"], // L
+    ["0100010011000000"], // Other L
     // отсуда свои фигуры
-    /*["0HH0HH000H000000"], // H 1
-    ["II00II00I0000000"], // I 2
-    ["JJ00J000J000J000"], // J 3
-    ["0K000K00KK00K000"], // K 4
-    ["0L000L00LLL00000"], // L 5
-    ["MMM0M0M000000000"], // M 6
-    ["NNN0N000N0000000"], // N 7
-    ["00O00OO0OO000000"], // O 8
-    ["0P00PPP00P000000"], // P 9
-    ["Q000QQ00Q000Q000"], // Q 10
-    ["0RR00R00RR000000"], // R 11*/
+    /*["0110110001000000"], // H 1
+    ["1100110010000000"], // I 2
+    ["1100100010001000"], // J 3
+    ["0100010011001000"], // K 4
+    ["0100010011100000"], // L 5
+    ["1110101000000000"], // M 6
+    ["1110100010000000"], // N 7
+    ["0010011011000000"], // O 8
+    ["0100111001000000"], // P 9
+    ["1000110010001000"], // Q 10
+    ["0110010011000000"], // R 11*/
   ],
-  newFigureLine = "AAAA000000000000", // Line
-  newFigureSquare = "A000A000A000A000", // квадрат
-  newFigureT = "A000A000A000A000", // T образная
-  newFigureL = "A000A000A000A000", // L
-  newFigureOtherL = "A000A000A000A000", // L в другую сторону
-  newFigureZ = "A000A000A000A000"; //
+  newFigureLine = "1000100010001000", // Line
+  newFigureSquare = "1100110000000000", // квадрат
+  newFigureT = "0100111000000000", // T образная
+  newFigureL = "1000100011000000", // L
+  newFigureOtherL = "0100010011000000", // Other L
+  newFigureZ = "1100011000000000",
+  //testFig = "1011100110011111";
+  //
+  testFig = "1111100110010001";
 
-const constructorGame = new ConstructorGame(allFigures, newFigureLine);
+const constructorGame = new ConstructorGame(allFigures, testFig);
 
-console.log("Проверка на уникальность: " + constructorGame.checkUnique() == 0 ? "уникальная" : "неуникальная");
+constructorGame.isCorrect();
 
 window.gaconstructorGameme = constructorGame;
