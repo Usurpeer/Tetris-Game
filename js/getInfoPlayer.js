cca3ff5c2578226c45044c74ce8b1d30087fc0ac
Svetlana <hristich.svetlana@gmail.com>
2023-12-07 01:07:10 +0400

    Попытки сделать всплывающее окно игрока

--- js/getInfoPlayer.js
+++ js/getInfoPlayer.js
@@ -1,11 +1,11 @@
 import script_cookie from "./get_cookies.js";
 
 const loginMenu = document.getElementById("login");
-const ratingScore = document.getElementById("rationgScore");
-const ratingTime = document.getElementById("rationgTime");
+const ratingScore = document.getElementById("ratingScore");
+const ratingTime = document.getElementById("ratingTime");
 
 loginMenu.textContent = "Логин: " + script_cookie("login");
 
-ratingScore.textContent = "Очков: " + script_cookie("ratingScore");
+ratingScore.textContent = "Рейтинг по очкам: \n" + script_cookie("ratingScore");
 
-ratingTime.textContent = "Очков: " + script_cookie("ratingTime");
+ratingTime.textContent = "Рейтинг по времени: \n" + script_cookie("ratingTime");
